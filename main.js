55759bf5bdbe0982bddf31453af8636676a33cf5
Shreesh-code <83659368+Shreesh-code@users.noreply.github.com>
2022-04-17 15:54:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,749 @@
+
+
+
+
+window.addEventListener("keydown",keydown );
+
+function keydown(e) {
+     keypress = e.keyCode;
+    console.log(keypress);
+
+    //arrow keys//
+
+    if (keypress=='37'){
+        Arrow();
+        console.log("arrow key pressed");
+    }
+
+    if (keypress=='38'){
+        Arrow();
+        console.log("arrow key pressed");
+    }
+
+ if (keypress=='39'){
+        Arrow();
+        console.log("arrow key pressed");
+    }
+
+    if (keypress=='40'){
+        Arrow();
+        console.log("arrow key pressed");
+    }
+
+//arrow keys//
+
+
+//special keys//
+
+if (keypress=='18'){
+    spec();
+console.log("special key pressed");
+}
+
+    if (keypress=='18'){
+    spec();
+ console.log("special key pressed");
+    }
+
+ if (keypress=='18'){
+   spec();
+console.log("special key pressed");
+ }
+
+    //special keys//
+    
+    
+    //number keys//
+
+if (keypress=='48'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='49'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='50'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='51'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='52'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='53'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='54'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='55'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='56'){
+    num();
+console.log("number key pressed");
+}
+
+if (keypress=='57'){
+    num();
+console.log("number key pressed");
+}
+
+//number keys//
+            
+
+//alphabet keys//
+
+if (keypress=='97'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='98'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='99'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='100'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='101'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='102'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='103'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='104'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='105'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='106'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='107'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='108'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='109'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='110'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='111'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='112'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='113'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='114'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='115'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='116'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='117'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='119'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='120'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='121'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='122'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='118'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='65'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='66'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='67'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='68'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='69'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='70'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='71'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+
+if (keypress=='72'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='73'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='74'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='75'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='76'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='77'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='78'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='79'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='80'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='81'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='82'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='83'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='84'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='85'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='86'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='87'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='88'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='89'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='90'){
+    letter();
+console.log("alphabet key pressed");
+}
+
+//alphabet keys//
+
+//other keys//
+
+
+if (keypress=='00'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='01'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='02'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='03'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='04'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='05'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='06'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='07'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='08'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='09'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='10'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='11'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='12'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='13'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='14'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='15'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='16'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='17'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='18'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='19'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='20'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='21'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='22'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='23'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='24'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='25'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='26'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='27'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='28'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='29'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='30'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='31'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='127'){
+    other();
+console.log("alphabet key pressed");
+}
+
+//other keys//
+
+//symbol//
+
+if (keypress=='33'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='34'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='35'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='36'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='37'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='38'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='39'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='40'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='41'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='42'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='43'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='44'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='45'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='46'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='47'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='59'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='60'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='61'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='62'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='63'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='64'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='91'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='92'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='93'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='94'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='95'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='96'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='123'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='124'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='125'){
+    other();
+console.log("alphabet key pressed");
+}
+
+if (keypress=='126'){
+    other();
+console.log("alphabet key pressed");
+}
+
+
+//symbol//
+
+}
+
+
+function Arrow(){
+    document.getElementById("ans").innerHTML="arrow key was pressed"
+}
+
+function spec(){
+    document.getElementById("ans").innerHTML="special key was pressed"
+}
+
+function num(){
+    document.getElementById("ans").innerHTML="number key was pressed"
+}
+
+function letter(){
+    document.getElementById("ans").innerHTML="alphabet key was pressed"
+}
+
+function other(){
+    document.getElementById("ans").innerHTML="other key was pressed"
+}
+
+
